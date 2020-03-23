9474a80a53a5ae3ad978aed08f956b5b42964437
Tieda Wei <weitieda@gmail.com>
2020-03-22 23:53:05 -0400

    update footer

--- Sources/Website/Node/Node+Footer.swift
+++ Sources/Website/Node/Node+Footer.swift
@@ -15,8 +15,24 @@ extension Node where Context == HTML.BodyContext {
             .class("footer"),
             .div(
                 .text("© \(currentYear) \(site.name). Written in Swift")
-            )
+            ),
+            .script(.src("https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js")),
+            .script(.src("https://www.gstatic.com/firebasejs/7.12.0/firebase-analytics.js")),
+            .script("""
+                   var firebaseConfig = {
+                     apiKey: "AIzaSyCM38prEWEDWkuumMhe6w0hFQgJlfDCNVM",
+                     authDomain: "tieda-website.firebaseapp.com",
+                     databaseURL: "https://tieda-website.firebaseio.com",
+                     projectId: "tieda-website",
+                     storageBucket: "tieda-website.appspot.com",
+                     messagingSenderId: "298663877799",
+                     appId: "1:298663877799:web:f8ed579a054f34ac364f82",
+                     measurementId: "G-LYQM6ZC04H"
+                   };
+                   // Initialize Firebase
+                   firebase.initializeApp(firebaseConfig);
+                   firebase.analytics();
+                """)
         )
     }
 }
-
