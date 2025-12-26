e51efa1bf022373148ca36578be285be2978ab06
Tieda Wei <weitieda@gmail.com>
2025-12-26 13:12:58 -0500

    Extract name title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import NameTittle from './components/NameTittle'
 import SocialIcons from './components/SocialIcons'
 
 function App() {
   return (
     <>
       <div className="page">
-        <h1>Tieda Wei</h1>
+        <NameTittle />
         <h3>I like to build stuff.</h3>
         <SocialIcons />
       </div>
---
+++ src/components/NameTittle.tsx
@@ -0,0 +1,17 @@
+function NameTittle() {
+  return (
+    <h1
+      style={{
+        margin: 0,
+        paddingTop: '10vh',
+        fontSize: '2em',
+        fontWeight: 600,
+        marginBottom: '20px',
+      }}
+    >
+      Tieda Wei
+    </h1>
+  )
+}
+
+export default NameTittle
