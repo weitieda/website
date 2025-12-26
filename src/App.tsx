6a4caaa3592053e20a09c887f4571ff425a25a89
Tieda Wei <weitieda@gmail.com>
2025-12-25 23:15:20 -0500

    Extract social icons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import SocialIcons from './components/SocialIcons'
 
 function App() {
   return (
@@ -6,31 +7,7 @@ function App() {
       <div className="page">
         <h1>Tieda Wei</h1>
         <h3>I like to build stuff.</h3>
-        <div className="icons">
-          <a
-            href="https://github.com/weitieda"
-            target="_blank"
-            rel="noreferrer"
-            className="github-icon"
-          >
-            <i className="fab fa-github fa-social-icons" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/tieda"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-linkedin-in fa-social-icons" />
-          </a>
-          <a
-            href="https://www.instagram.com/twei3"
-            target="_blank"
-            rel="noreferrer"
-            className="ytb-icon"
-          >
-            <i className="fab fa-instagram fa-social-icons" />
-          </a>
-        </div>
+        <SocialIcons />
       </div>
     </>
   )
