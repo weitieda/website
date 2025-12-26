a76d4dbb264b1145d8b99d6d90b1c80f1917d9c4
Tieda Wei <weitieda@gmail.com>
2025-12-25 23:13:23 -0500

    Show all social icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,35 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Tieda Wei</h1>
-      <h3>I like to build stuff.</h3>
+      <div className="page">
+        <h1>Tieda Wei</h1>
+        <h3>I like to build stuff.</h3>
+        <div className="icons">
+          <a
+            href="https://github.com/weitieda"
+            target="_blank"
+            rel="noreferrer"
+            className="github-icon"
+          >
+            <i className="fab fa-github fa-social-icons" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tieda"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in fa-social-icons" />
+          </a>
+          <a
+            href="https://www.instagram.com/twei3"
+            target="_blank"
+            rel="noreferrer"
+            className="ytb-icon"
+          >
+            <i className="fab fa-instagram fa-social-icons" />
+          </a>
+        </div>
+      </div>
     </>
   )
 }
